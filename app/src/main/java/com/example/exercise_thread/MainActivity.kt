a7008d4bca2ffabfaaf4d28954eda7d975af2000
DatNguyen <tiendat08092001@gmail.com>
2022-08-08 17:28:23 +0700

    update

--- app/src/main/java/com/example/exercise_thread/MainActivity.kt
+++ app/src/main/java/com/example/exercise_thread/MainActivity.kt
@@ -1,138 +1,207 @@
 package com.example.exercise_thread
 
-
 import android.annotation.SuppressLint
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
+import android.graphics.Color
+import android.os.*
+import android.util.Log
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var numberIsShowing: Int = 0
+    private val random = Random()
+    private var number = 0
     lateinit var textNumber: TextView
     private lateinit var handler: Handler
-    private var isUpdate: Boolean = false
-    private var isClickPlusButton: Boolean = false
-    private var isClickMinusButton: Boolean = false
-    private lateinit var clickPlusBTNThread: Thread
-    private lateinit var clickMinusBTNThread: Thread
-    private lateinit var touchMinusThread: Thread
-    private lateinit var touchPlusThread: Thread
-    private lateinit var autoUpdateNumber: Thread
     private var y1: Float = 0F
     private var y2: Float = 0F
-
-
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        listenerHandler()
-        initThread()
-        textNumber = findViewById(R.id.textview_showing_number)
-        textNumber.setOnTouchListener { view, motionEvent ->
-            when (motionEvent.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    y1 = motionEvent.y
-                    isUpdate = false
-                    autoUpdateNumber.interrupt()
+    private lateinit var btnPlus: Button
+    private lateinit var btnMinus: Button
+    private var isPlus = true
+    private var isUpdate = false
+    private var isZero = false
+    private var isRun = false
+    private  var kc1: Float = 0F
+    private  var kc: Float = 0F
+    private  var kc2: Float = 0F
+    private var isTouch = false
+    private val updateNumberThread = Thread {
+        while (true) {
+            if (isRun) {
+                if (number > 0) {
+                    isPlus = false
+                } else if (number < 0) {
+                    isPlus = true
                 }
+            }
+        }
+    }
 
-                MotionEvent.ACTION_UP -> {
-                    isClickPlusButton = false
-                    isClickMinusButton = false
-                    isUpdate = true
-                    autoUpdateNumber.start()
-                    touchPlusThread.interrupt()
-                    touchMinusThread.interrupt()
+    private val mThread = Thread {
+        while (true) {
+            if (isUpdate) {
+                if (!isTouch) {
+                    countDownTimerLongClick.cancel()
+                    handler.sendEmptyMessage(MSG_UPDATE_NUMBER)
                 }
-
-                MotionEvent.ACTION_MOVE -> {
-                    y2 = motionEvent.y
-
-                    if (y1 > y2) {
-                        isClickPlusButton = true
-                        isUpdate = false
-                        touchPlusThread.start()
-                        autoUpdateNumber.interrupt()
-
-                    } else if (y1 < y2) {
-                        isClickMinusButton = true
-                        isUpdate = false
-                        touchMinusThread.start()
-                        autoUpdateNumber.interrupt()
-                    }
-                }
-
 
             }
+            Thread.sleep(30)
+        }
+    }
 
-
-            true
+    private var countDownTimer = object : CountDownTimer(2000, 500) {
+        override fun onTick(p0: Long) {
+            val tag = "Main"
+            Log.i(tag, "Current tick: $p0")
         }
 
-        val btnPlus = findViewById<Button>(R.id.btnPlus)
+        override fun onFinish() {
+            isPlus = !isPlus
+            isUpdate = true
+            isZero = true
+            isTouch = false
+            isRun = true
+        }
+    }
 
-        btnPlus.setOnTouchListener { view, motionEvent ->
-            when (motionEvent.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    isUpdate = false
-                    autoUpdateNumber.interrupt()
-                    isClickPlusButton = true
-                    clickPlusBTNThread.start()
-                }
-
-                MotionEvent.ACTION_UP -> {
-                    numberIsShowing += 1
-                    textNumber.text = numberIsShowing.toString()
-                    isUpdate = true
-                    isClickPlusButton = false
-                    clickPlusBTNThread.interrupt()
-                    autoUpdateNumber.start()
-                }
-            }
-            true
+    private var countDownTimerLongClick = object : CountDownTimer(1000, 500) {
+        override fun onTick(p0: Long) {
+            isUpdate = false
         }
 
-
-        val btnMinus = findViewById<Button>(R.id.btnMinus)
-        btnMinus.setOnTouchListener { view, motionEvent ->
-            when (motionEvent.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    isUpdate = false
-                    isClickMinusButton = true
-                    clickMinusBTNThread.start()
-                    autoUpdateNumber.interrupt()
-                }
-
-                MotionEvent.ACTION_UP -> {
-                    numberIsShowing -= 1
-                    textNumber.text = numberIsShowing.toString()
-                    isUpdate = true
-                    isClickMinusButton = false
-                    clickMinusBTNThread.interrupt()
-                    autoUpdateNumber.start()
-
-                }
-            }
-            true
+        override fun onFinish() {
+            isUpdate = true
         }
 
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        listenerHandler()
+
+        handleView()
+        mThread.start()
+        updateNumberThread.start()
+
+
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun handleView() {
+        textNumber = findViewById(R.id.textview_showing_number)
+        textNumber.setOnTouchListener { view, event ->
+            textNumber.onTouchEvent(event)
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    y1 = event.y
+                    isUpdate = false
+                    isRun = false
+                    isZero = false
+                    isTouch = true
+                }
+
+                MotionEvent.ACTION_UP -> {
+
+                    isUpdate = false
+                    isTouch = false
+                    countDownTimer.cancel()
+                    countDownTimer.start()
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    isUpdate = false
+                    isZero = false
+                    isTouch = false
+                    y2 = event.y
+
+
+
+                    if (y2 > y1) {
+                        isPlus = false
+                        updateNumber()
+//                        y1 = y2
+
+                    } else if (y2 < y1) {
+                        isPlus = true
+                        updateNumber()
+//                        y1 = y2
+                    }
+                }
+            }
+
+            true
+        }
+
+        btnPlus = findViewById(R.id.btnPlus)
+        btnPlus.setOnClickListener {
+            isRun = false
+            isUpdate = false
+            isTouch = false
+            isPlus = true
+            isZero = false
+            updateNumber()
+            countDownTimerLongClick.cancel()
+            countDownTimer.cancel()
+            countDownTimer.start()
+        }
+
+        btnPlus.setOnLongClickListener {
+            isUpdate = false
+            isRun = false
+            isPlus = true
+            isTouch = false
+            countDownTimer.cancel()
+            countDownTimerLongClick.cancel()
+            countDownTimerLongClick.start()
+
+            isUpdate = true
+            isZero = false
+            false
+        }
+
+        btnMinus = findViewById(R.id.btnMinus)
+        btnMinus.setOnClickListener {
+            isRun = false
+            isUpdate = false
+            isPlus = false
+            isZero = false
+            isTouch = false
+            updateNumber()
+            countDownTimerLongClick.cancel()
+            countDownTimer.cancel()
+            countDownTimer.start()
+        }
+
+        btnMinus.setOnLongClickListener {
+            isUpdate = false
+            isRun = false
+            isPlus = false
+
+            countDownTimer.cancel()
+            countDownTimerLongClick.cancel()
+            countDownTimerLongClick.start()
+            isTouch = false
+
+            isUpdate = true
+            isZero = false
+            false
+        }
+
+
+    }
 
     private fun listenerHandler() {
         handler = object : Handler(Looper.getMainLooper()) {
             override fun handleMessage(msg: Message) {
                 when (msg.what) {
                     MSG_UPDATE_NUMBER -> {
-                        textNumber.text = msg.arg1.toString()
+                        updateNumber()
                     }
 
                     MSG_UPDATE_NUMBER_DONE -> {
@@ -143,89 +212,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-    private fun plusThread(timeSleep: Long, delay: Long, isTouch: Boolean = false): Thread {
-        val a = Thread {
-            Thread.sleep(delay)
-            while (true) {
-                if (!isClickPlusButton) {
-                    break
-                }
-                numberIsShowing += 1
-                val message = Message()
-                message.what = MSG_UPDATE_NUMBER
-                message.arg1 = numberIsShowing
-                handler.sendMessage(message)
-                Thread.sleep(timeSleep)
-
-                if (isTouch) {
-                    break
-                }
-            }
+    private fun updateNumber() {
+        if (isZero && number == 0) {
+            return
         }
-        return a
-    }
 
-    private fun minusThread(timeSleep: Long, delay: Long, isTouch: Boolean = false): Thread {
-        val a = Thread {
-            Thread.sleep(delay)
-            while (true) {
-                if (!isClickMinusButton) {
-                    break
-                }
-                numberIsShowing -= 1
-                val message = Message()
-                message.what = MSG_UPDATE_NUMBER
-                message.arg1 = numberIsShowing
-                handler.sendMessage(message)
-                Thread.sleep(timeSleep)
-
-                if (isTouch) {
-                    break
-                }
-            }
-
+        if (isPlus) {
+            number++
+        } else {
+            number--
         }
-        return a
-    }
 
-    private fun autoUpdateThread(): Thread {
-        val a = Thread {
-            Thread.sleep(2000)
-            while (isUpdate) {
-                if (numberIsShowing == 0) {
-                    break
-                }
+        val color = Color.argb(255, random.nextInt(50), random.nextInt(100), random.nextInt(150))
+        textNumber.text = number.toString()
+        textNumber.setTextColor(color)
 
-                if (numberIsShowing > 0) {
-                    val message = Message()
-                    numberIsShowing -= 1
-                    message.arg1 = numberIsShowing
-                    message.what = MSG_UPDATE_NUMBER
-                    handler.sendMessage(message)
-                    Thread.sleep(50)
-                } else if (numberIsShowing < 0) {
-                    val message = Message()
-                    numberIsShowing += 1
-                    message.arg1 = numberIsShowing
-                    message.what = MSG_UPDATE_NUMBER
-                    handler.sendMessage(message)
-                    Thread.sleep(50)
-                }
-
-
-            }
-            handler.sendEmptyMessage(MSG_UPDATE_NUMBER_DONE)
-        }
-        return a
-    }
-
-    private fun initThread() {
-        clickPlusBTNThread = plusThread(50, 1500, false)
-        clickMinusBTNThread = minusThread(50, 1500, false)
-        touchMinusThread = minusThread(50, 0, true)
-        touchPlusThread = plusThread(50, 0, true)
-        autoUpdateNumber = autoUpdateThread()
     }
 
     companion object {
@@ -233,3 +234,4 @@ class MainActivity : AppCompatActivity() {
         private const val MSG_UPDATE_NUMBER_DONE = 101
     }
 }
+
